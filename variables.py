88af9336d6e5605ca9eebd0ed652b4669669110f
SketchTurner74 <w_maker@inbox.ru>
2024-09-02 14:35:34 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+Homework=12
+Hours=1.5
+Course='Python'
+One_Task_Time=Hours/Homework
+print(f'Курс:{Course}, всего задач:{Homework}, затрачено часов:{Hours}, среднее время выполнения {One_Task_Time} часа.')
